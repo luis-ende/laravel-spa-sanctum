77427f483cbb3a5fab9de712392a2e77f74d3ab3
Jose Luis Salazar <lsalazar@gmx.de>
2024-02-23 09:35:23 -0600

    Create Investment Factory

---
+++ database/factories/InvestmentFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Investment>
+ */
+class InvestmentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => 1,
+            'strategy_id' => 1,
+            'successful' => true,
+            'amount' => 1000,
+            'returns' => 2000,
+        ];
+    }
+}
