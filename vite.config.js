5c701c5df1315306f00e0184dcf8692432b61187
Jose Luis Salazar <lsalazar@gmx.de>
2024-02-23 16:23:18 -0600

    Install Bootstrap 5

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import laravel from 'laravel-vite-plugin'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
     plugins: [
@@ -30,4 +31,9 @@ export default defineConfig({
             },
         })
     ],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
 });
